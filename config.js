9e7cd364612e7838d6d82f585ded2c3b843f50c6
Cuowg2412 <157120359+Cuowg2412@users.noreply.github.com>
2024-10-24 18:34:13 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Chị Bphuong';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng Phương bóng bảy';
+const contentCard = 'E Cường Mafia internet trùm kéo view số 1 VN chúc chị Phương bình an, chúc chị mạnh khỏe, và chúc gia đình chị Phương vạn sự như ý tỷ sự như mơ triệu triệu bất ngờ và ngâp tràn may mắn. Chúc chị Phương MÃi ĐỈNH MÃI ĐỈNH MÃI ĐỈNH LUÔN!!!';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
